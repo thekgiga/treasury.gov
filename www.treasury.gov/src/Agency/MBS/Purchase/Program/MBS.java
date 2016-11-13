39c8c5f6ea54c36118ac5f02b2780fb942214c8d
Gordan <Gordan@Thekgiga>
2016-11-13 02:48:24 +0100

    implemented basic download function of .xls .xlsx and .csv files on part of website. Also developed Table scrape tool

--- www.treasury.gov/src/Agency/MBS/Purchase/Program/MBS.java
+++ www.treasury.gov/src/Agency/MBS/Purchase/Program/MBS.java
@@ -1,5 +1,123 @@
 package Agency.MBS.Purchase.Program;
 
-public class MBS {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MBS{
+	
+	public static final String USER_AGENT = "Mozilla/5.0";	
+	public static String baseUrl = "https://www.treasury.gov";
+	public static String url = "https://www.treasury.gov/resource-center/data-chart-center/Pages/mbs-purchase-program.aspx";
+	public static ArrayList<String> listToBeDownlaoded = new ArrayList<>();
+	public static void getFiles(){
+		
+		String pageSource;
+		
+			try {
+				pageSource = sendGet(url);
+				Document doc =  Jsoup.parse(pageSource);
+				//System.out.println(doc);
+				Elements elements = doc.getElementsByTag("a");
+
+				
+				for (Element element : elements) {
+					String attHref = element.attr("href");
+					//System.out.println(attHref);
+					if (attHref.endsWith("xls")||
+							attHref.endsWith("xlsx")||
+								attHref.endsWith("csv")	) {
+						listToBeDownlaoded.add(baseUrl+attHref);
+						//String extension = attHref.substring(attHref.lastIndexOf('.'),attHref.length());
+						//downloadFile(baseUrl+attHref, extension);
+					}
+				}
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+	}
+	
+	public static void listAll(){
+		for (String string : listToBeDownlaoded) {
+			System.out.println(string);
+		}
+	}
+	public static String sendGet(String urlUrl) throws Exception {
+
+		String url = urlUrl;
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		return response.toString();
+
+	}
+	
+	public static void downloadFile(ArrayList<String> downloadLink) {
+		int i =0;
+		for (String string : downloadLink) {
+			try {
+				String extension1 = string.substring(string.lastIndexOf('.'),string.length());
+				
+				URL website = new URL(string);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				
+				FileOutputStream fos = new FileOutputStream("FileName "+i+extension1);
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				i++;
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				System.err.println("file not exist on :" + downloadLink);
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+			
+
+	}
+
+
 
 }
--- www.treasury.gov/src/www/treasury/gov/ScrapeMain.java
+++ www.treasury.gov/src/www/treasury/gov/ScrapeMain.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import Agency.MBS.Purchase.Program.MBS;
+
 public class ScrapeMain {
 	
 	private static final String USER_AGENT = "Mozilla/5.0";
@@ -44,92 +46,97 @@ public class ScrapeMain {
 	}
 	
 	public static void main(String[] args){
+		MBS.getFiles();
+		MBS.listAll();
+		MBS.downloadFile(MBS.listToBeDownlaoded);
+		
+		
 		
 	   
-
-		
-		ScrapeTables scrapeTables = new ScrapeTables();
-		int counter = 0;
-		for (String urlPart : scrapeTables.linksToScrape) {
-			String name = scrapeTables.fileNames.get(counter);
-			
-			try {
-				FileWriter writ = new FileWriter(name+".csv");
-				Tabel t = new Tabel(urlPart);
-		
-				writ.write(t.columnsFormated+"\n");
-				writ.write(t.columnsSecondRowFormated+"\n");
-				for (String string : t.data) {
-					writ.write(string+"\n");
-				}
-				writ.close();
-				//System.out.println(t.columns);
-				//System.out.println(t.columnsSecondRow);
-				//for (String string : t.data) {
-				//	System.out.println(string);
-					
-					
-				//}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-			counter++;
-			
-		}
-		
-		
-		
-//		ArrayList<String> listOfLinksForTable = getLinksForTableScrape();
 //
-//		ArrayList<String> tableData = new ArrayList<>();
-//		ArrayList<String> lineOfTableData = new ArrayList<>();
 //		
-//		
-//		for (String linkForTable : listOfLinksForTable) {
+//		ScrapeTables scrapeTables = new ScrapeTables();
+//		int counter = 0;
+//		for (String urlPart : scrapeTables.linksToScrape) {
+//			String name = scrapeTables.fileNames.get(counter);
 //			
-//			String url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/"+linkForTable+"all";
-//			
-//			
-//			Document doc = null;
 //			try {
-//				doc = Jsoup.parse(sendGet(url));
-//			} catch (Exception e) {
-//				
+//				FileWriter writ = new FileWriter(name+".csv");
+//				Tabel t = new Tabel(urlPart);
+//		
+//				writ.write(t.columnsFormated+"\n");
+//				writ.write(t.columnsSecondRowFormated+"\n");
+//				for (String string : t.data) {
+//					writ.write(string+"\n");
+//				}
+//				writ.close();
+//				//System.out.println(t.columns);
+//				//System.out.println(t.columnsSecondRow);
+//				//for (String string : t.data) {
+//				//	System.out.println(string);
+//					
+//					
+//				//}
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
 //				e.printStackTrace();
 //			}
 //			
-//			Elements elements = doc.getElementsByClass("text_view_data");
-//				
-//			for (Element el : elements) {
-//				
-//				tableData.add(el.text());
-//			}
 //			
-//			int count = 0;
-//			String line = "";
+//			counter++;
 //			
-//			for (String s : tableData) {
-//				
-//				line = line + "#" + s;
-//				count++;
-//				if (count == 12) {
-//					lineOfTableData.add(line);
-//					count = 0;
-//					line = "";						
-//				}
-//				
-//			}
-//			
-//			for (String s : lineOfTableData) {
-//				System.out.println(s);
-//			}
 //		}
 //		
 //		
 //		
+////		ArrayList<String> listOfLinksForTable = getLinksForTableScrape();
+////
+////		ArrayList<String> tableData = new ArrayList<>();
+////		ArrayList<String> lineOfTableData = new ArrayList<>();
+////		
+////		
+////		for (String linkForTable : listOfLinksForTable) {
+////			
+////			String url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/"+linkForTable+"all";
+////			
+////			
+////			Document doc = null;
+////			try {
+////				doc = Jsoup.parse(sendGet(url));
+////			} catch (Exception e) {
+////				
+////				e.printStackTrace();
+////			}
+////			
+////			Elements elements = doc.getElementsByClass("text_view_data");
+////				
+////			for (Element el : elements) {
+////				
+////				tableData.add(el.text());
+////			}
+////			
+////			int count = 0;
+////			String line = "";
+////			
+////			for (String s : tableData) {
+////				
+////				line = line + "#" + s;
+////				count++;
+////				if (count == 12) {
+////					lineOfTableData.add(line);
+////					count = 0;
+////					line = "";						
+////				}
+////				
+////			}
+////			
+////			for (String s : lineOfTableData) {
+////				System.out.println(s);
+////			}
+////		}
+////		
+////		
+////		
 	}
 
 }
